5a073a2c52ba810c9d535bf069c67c327e34e5f7
Aso Yusuke <aso.aso.1227@i.softbank.jp>
2021-02-27 06:18:44 +0000

    [add]bookers-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,22 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(list_params)
+
+    book.save
+
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_paramms
+    params.require(:book).permit(:本のタイトル, :感想)
   end
 end
