aea9fe5a50b720eaf5bad1d55be45518f4652eb9
Aso Yusuke <aso.aso.1227@i.softbank.jp>
2021-03-01 12:33:41 +0000

    [add]bookers-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,42 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
-    @book = Book.new
   end
 
   def create
-    book = Book.new(list_params)
-
-    book.save
-
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
-  def book_paramms
-    params.require(:book).permit(:本のタイトル, :感想)
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_02_27_032834) do
 
   create_table "books", force: :cascade do |t|
-    t.string "本のタイトル"
-    t.text "感想"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
