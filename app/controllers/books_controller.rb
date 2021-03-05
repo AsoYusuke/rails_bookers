bae597d1281ee46d6d5fa82f2a54669f65f5512d
Aso Yusuke <aso.aso.1227@i.softbank.jp>
2021-03-05 08:44:45 +0000

    [add]bookers-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,27 +9,34 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = Book.edit(book_params)
     if @book.save
-      redirect_to @book, notice: 'Book was successfully updated.'
-    else
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to books_path
       @books = Book.all
       render :index
     end
   end
 
-  def edit
-    @book = Book.find(params[:id])
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
+
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to @book, notice: 'Book was successfully destroyed.'
   end
 
   private
